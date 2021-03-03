332f86f370aacb06994dd20dd3de1598cea94406
elham <elham.ne94@gmail.com>
2021-03-03 20:36:11 +0330

    Media (avatars and voice) section of child page

--- src/components/Child/ChildPage.js
+++ src/components/Child/ChildPage.js
@@ -2,35 +2,13 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import AudioPlayer from 'material-ui-audio-player';
+import MediaSection from './MediaSection';
 
 const useStyles = makeStyles({
   root: {
-    padding: 4,
+    padding: 12,
     width: '100%',
-  },
-  mediaCard: {
-    display: 'flex',
-    flexDirection: 'row-reverse',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    flexWrap: 'wrap',
-  },
-  media: {
-    width: 100,
-    height: 100,
-    borderRadius: '50%',
-    margin: 20,
-  },
-});
-
-const audioStyle = makeStyles({
-  root: {
-    backgroundColor: 'rgba(0, 0, 0, 0.05)',
-    boxShadow: 'none',
   },
 });
 
@@ -39,47 +17,17 @@ const ChildPage = () => {
 
   return (
     <div className={classes.root}>
-      <Grid container direction="column" spacing={1}>
-        <Grid item>
-          <Card className={classes.mediaCard}>
-            <Box
-              display="flex"
-              flexDirection="row-reverse"
-              alignItems="center"
-              mx={1}
-            >
-              <CardMedia
-                className={classes.media}
-                component="img"
-                src="https://sayapp.company/files/1-child/1-avatar_0010010001.png"
-                title="Setareh"
-              />
-              <CardContent>Avatar</CardContent>
-            </Box>
-            <Box
-              display="flex"
-              flexDirection="row-reverse"
-              alignItems="center"
-              mx={1}
-            >
-              <CardMedia
-                className={classes.media}
-                component="img"
-                src="https://sayapp.company/files/1-child/1-sleptAvatar_0010010001.png"
-                title="Setareh"
-              />
-              <CardContent>Slept Avatar</CardContent>
-            </Box>
-            <Box display="flex" justifyContent="center" flexGrow={1} mx={3}>
-              <AudioPlayer
-                src="https://sayapp.company/files/1-child/1-voice_0010010001.mp3"
-                download
-                useStyles={audioStyle}
-              />
-            </Box>
-          </Card>
+      <Grid container direction="column" spacing={3}>
+        <Grid
+          container
+          item
+          direction="row-reverse"
+          justify="space-between"
+          spacing={2}
+        >
+          <MediaSection />
         </Grid>
-        <Grid item>
+        <Grid item spacing={2}>
           <Card>
             <CardContent></CardContent>
           </Card>
---
+++ src/components/Child/MediaSection.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
+import AudioPlayer from 'material-ui-audio-player';
+
+const useStyles = makeStyles({
+  mediaCard: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    textAlign: 'center',
+    height: 190,
+  },
+});
+
+const audioStyle = makeStyles({
+  root: {
+    backgroundColor: 'rgba(0, 0, 0, 0.05)',
+    boxShadow: 'none',
+    width: '90%',
+    margin: 12,
+    marginTop: 20,
+  },
+});
+
+const MediaSection = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Grid item xl={2} lg={2} sm={6} xs={12}>
+        <Card className={classes.mediaCard}>
+          <CardMedia
+            component="img"
+            src="https://sayapp.company/files/1-child/1-avatar_0010010001.png"
+            title="Setareh"
+          />
+          <CardContent>Avatar</CardContent>
+        </Card>
+      </Grid>
+      <Grid item xl={2} lg={2} sm={6} xs={12}>
+        <Card className={classes.mediaCard}>
+          <CardMedia
+            component="img"
+            src="https://sayapp.company/files/1-child/1-sleptAvatar_0010010001.png"
+            title="Setareh"
+          />
+          <CardContent>Slept Avatar</CardContent>
+        </Card>
+      </Grid>
+      <Grid item xl={8} lg={8} sm={12} xs={12}>
+        <Card className={classes.mediaCard}>
+          <AudioPlayer
+            src="https://sayapp.company/files/1-child/1-voice_0010010001.mp3"
+            download
+            useStyles={audioStyle}
+          />
+          <CardContent>Voice</CardContent>
+        </Card>
+      </Grid>
+    </>
+  );
+};
+
+export default MediaSection;
